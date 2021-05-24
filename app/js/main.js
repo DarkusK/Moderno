057c9f2b0ae3986586e20a36ebd00db224da7b70
DarkusK <darkus.darkus16@gmail.com>
2021-05-24 14:15:33 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,15 @@
 $(function(){
 
 
+    var mixer = mixitup('.products__inner-box');
+
+    $('.product-slider__inner').slick({
+        dots:true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+    })
+
 });
+
